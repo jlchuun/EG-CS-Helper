8852aaea42a06d752f2ef8304e876e49cdf76fb8
Joshua Chung <jlchuun@gmail.com>
2023-05-23 15:23:04 -0700

    feat: initial function outline

---
+++ ProcessGameState.py
@@ -0,0 +1,18 @@
+
+class ProcessGameState:
+    def __init__(self, filePath):
+        # load data from file path
+
+    def loadData(self, filePath):
+        # load data from file path
+        # convert parquet into data frame
+
+        # perform ETL into readable data
+
+    def validBoundary(self, x, y):
+        # check if give (x, y) is within map boundaries
+        # return True or False
+
+    def getWeapons(self):
+        # parse inventory json for weapon classes
+        # return list of weapons
