d35763f622bf1add69c2949448af2386e3157e90
Joshua Chung <jlchuun@gmail.com>
2023-05-24 20:45:04 -0700

    feat: implemented valid_boundary()

--- ProcessGameState.py
+++ ProcessGameState.py
@@ -18,8 +18,23 @@ class ProcessGameState:
     # player name (string)
     # (xBound, yBound) x and y boundaries as tuples ((int, int), (int, int))
     # Returns if player was within given x and y boundaries at any point
-    # def valid_boundary(self, round, seconds, player, xBound, yBound):
+    def valid_boundary(self, round, seconds, player, xBound, yBound):
+        # query for player in round, second
+        query_string = "round_num == {} and seconds == {} and player == '{}'".format(round, seconds, player)
+        query_res = self.data.query(query_string)
 
+        x_min, x_max = xBound[0], xBound[1]
+        y_min, y_max = yBound[0], yBound[1]
+
+        for _, row in query_res.iterrows():
+            x, y = row['x'], row['y']
+            print(type(x))
+            # return True on first instance of player within bounds
+            if (x_min <= x) & (x <= x_max) & (y_min <= y) & (y <= y_max):
+                print('Within bounds!')
+                return True
+        print('Not in bounds')
+        return False
 
     # def get_weapons(self):
     #     parse inventory json for weapon classes
